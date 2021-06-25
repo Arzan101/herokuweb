170d9f8fe70d8a48b983fc26097969ef607a3f55
Arzan101 <84129750+Arzan101@users.noreply.github.com>
2021-06-25 18:14:29 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,1 @@
-const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost/arzan1',
- {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true
- }).then(()=>{
-     console.log("connection successfull")
- }).catch((error)=>{
-     console.log(error);
- })
